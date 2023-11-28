9c5fa835b7f30e2ede53fa3bf715eafd888081b6
Lucija <lucija.strejcek@fer.hr>
2023-11-28 11:57:56 +0100

    Add vehicle store and update ui

--- src/components/VehicleList/index.jsx
+++ src/components/VehicleList/index.jsx
@@ -1,3 +1,4 @@
+import vehicleStore from "@/stores/VehicleStore";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import {
   Button,
@@ -9,15 +10,16 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 export function VehicleList() {
-  const vehicles = [
-    { Id: 1, Name: "Toyota", Abrv: "TYT" },
-    { Id: 2, Name: "Audi", Abrv: "AD" },
-    { Id: 3, Name: "Mercedes Benz", Abrv: "MB" },
-    { Id: 4, Name: "Alfa Romeo", Abrv: "ALRO" },
-    { Id: 5, Name: "Citroen", Abrv: "CTR" },
-  ];
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    vehicleStore
+      .fetchVehicles("")
+      .then(() => setData(vehicleStore.vehicleData));
+  }, []);
+
   return (
     <TableContainer mt={"20px"}>
       <Table variant="simple" size="lg">
@@ -30,7 +32,7 @@ export function VehicleList() {
           </Tr>
         </Thead>
         <Tbody>
-          {vehicles.map((vehicle) => {
+          {data.map((vehicle) => {
             return (
               <Tr key={vehicle.Id}>
                 <Td>{vehicle.Id}</Td>
---
+++ src/stores/VehicleStore.js
@@ -0,0 +1,41 @@
+import { makeObservable, runInAction } from "mobx";
+import { VehicleService } from "../services/VehicleService";
+
+class VehicleStore {
+  vehicleService = new VehicleService();
+  vehicleData = [];
+
+  constructor() {
+    makeObservable(this);
+  }
+
+  fetchVehicles = async (paramsData) => {
+    try {
+      var params = {
+        sortBy: paramsData.sortBy,
+        orderBy: paramsData.orderBy,
+        limit: paramsData.limit,
+        skip: paramsData.skip,
+        Id: paramsData.Id,
+        Name: paramsData.Name,
+        Abrv: paramsData.Abrv,
+        MakeId: paramsData.MakeId,
+      };
+      const filteredParams = Object.fromEntries(
+        Object.entries(params).filter(([key, value]) => value !== undefined)
+      );
+      const urlParams = new URLSearchParams(Object.entries(filteredParams));
+      const data = await this.vehicleService.getVehicles(urlParams);
+      runInAction(() => {
+        this.vehicleData = data;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.status = "error";
+      });
+    }
+  };
+}
+
+const vehicleStore = new VehicleStore();
+export default vehicleStore;
